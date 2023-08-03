879e37a9632133728612f629438621d66fdba818
Juan1Esteban <juanpismanrique@gmail.com>
2023-08-03 16:03:56 -0500

    Precarga de Modulos

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -6,7 +6,6 @@ import { WebsiteRoutingModule } from './website-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MycartComponent } from './pages/mycart/mycart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -21,7 +20,6 @@ import { NavComponent } from './components/nav/nav.component';
   declarations: [
     NavComponent,
     HomeComponent,
-    CategoryComponent,
     MycartComponent,
     LoginComponent,
     RegisterComponent,
